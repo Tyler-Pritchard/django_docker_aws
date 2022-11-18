d38498195fefdd4a81b41df315466176adaec5e5
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-11-17 20:39:03 -0800

    updated ec2 uri, settings.py

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rd_q8=#5%)xlhl8!b8o410apqror1(3^2j6m3jr#xr&!+!8%84
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-54-185-82-2.us-west-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
